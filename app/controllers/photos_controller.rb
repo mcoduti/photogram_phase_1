af062407e61529918aecaccb29cee1573ec742de
mcoduti <matthew.coduti@gmail.com>
2015-07-28 00:21:16 -0500

    completed last part of hw 5

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,32 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def new_form
+
+
+
+  end
+
+  def show
+    @photo_db_id = params["id"]
+    m = Photo.new
+
+    @all_entries = Photo.all
+    p =Photo.find(@photo_db_id)
+
+    @p_source = p.source
+    @p_caption = p.caption
+
+    @db_count = @all_entries.count
+  end
+
+  def destroy
+    @photo_db_id = params["id"]
+    p = Photo.find(@photo_db_id)
+
+    p.destroy
+
+  end
+
 
 end
